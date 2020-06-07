50c93252468e714bb240057ebc9b6c1f4cf61a1c
mtmercurio <mtmercurio@gmail.com>
2020-06-06 20:13:31 -0400

    editing front page

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,63 @@
     <figure class="image is-16by9">
       <img src="~assets/studios.jpg" alt="main">
     </figure>
+    <section class="hero is-light">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            STUDIOS
+          </h1>
+          <h2 class="subtitle">
+            Customize your space to your music making needs
+          </h2>
+        </div>
+      </div>
+    </section>
     <figure class="image is-16by9">
       <img src="~assets/theater.jpg" alt="main">
     </figure>
+    <section class="hero is-light">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            THEATERS
+          </h1>
+          <h2 class="subtitle">
+            A personalized movie-going experience
+          </h2>
+        </div>
+      </div>
+    </section>
     <figure class="image is-16by9">
       <img src="~assets/acoustics.jpg" alt="main">
     </figure>
+    <section class="hero is-light">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            ACOUSTICS
+          </h1>
+          <h2 class="subtitle">
+            Dial into your sound
+          </h2>
+        </div>
+      </div>
+    </section>
     <figure class="image is-16by9">
       <img src="~assets/furniture.jpg" alt="main">
     </figure>
+    <section class="hero is-light">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            FURNITURE
+          </h1>
+          <h2 class="subtitle">
+            Built with you in mind
+          </h2>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
