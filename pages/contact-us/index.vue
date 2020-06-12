906bd8cd08124d68398019d16bf7b547e8e0f946
mtmercurio <mtmercurio@gmail.com>
2020-06-12 17:08:25 -0400

    added Contact Us page

--- pages/contact-us/index.vue
+++ pages/contact-us/index.vue
@@ -1,7 +1,39 @@
 <template>
-  <div class="container">
-    hello
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title is-2">
+        Contact Us
+      </h1>
+      <h2 class="subtitle">
+        We're available 9am-8pm Mon-Sun
+      </h2>
+      <br/>
+      <h3 class="title is-4">
+        Warehouse Address
+      </h3>
+      <h4 class="subtitle">
+        4756 Hammermill Rd Tucker, GA 30084
+      </h4>
+      <br/>
+      <h3 class="title is-4">
+        Phone
+      </h3>
+      <h4 class="subtitle">
+        <a href="tel:7703782866">
+          (770) 378-2866
+        </a>
+      </h4>
+      <br/>
+      <h3 class="title is-4">
+        Email
+      </h3>
+      <h4 class="subtitle">
+        <a href="mailto:mike@magcsf.com">
+          sales@magcsf.com
+        </a>
+      </h4>
+    </div>
+  </section>
 </template>
 
 <script>
