04911d9ffcc27559629b48745168471423d6be60
mtmercurio <mtmercurio@gmail.com>
2020-06-12 16:31:45 -0400

    finalizing front page and some spacing

--- layouts/default.vue
+++ layouts/default.vue
@@ -41,16 +41,37 @@
       </div>
     </section>
     <footer class="footer">
-      <div class="columns is-vcentered">
+      <div class="columns is-vcentered is-hidden-mobile">
         <div class="column is-narrow">
           <figure class="image is-128x128">
             <img src="~assets/MAG.png">
           </figure>
         </div>
         <div class="column">
-          <p>(770) 378-2866</p>
-          <p>mike@magcsf.com</p>
+          <a class="content is-large" href="tel:7703782866">
+            (770) 378-2866
+          </a>
+          <p class="content is-large" href="mailto:mike@magcsf.com">
+            sales@magcsf.com
+          </p>
         </div>
+      </div>
+      <div class="container has-text-centered is-hidden-tablet">
+        <div class="content is-flex is-horizontal-center">
+          <figure class="image is-128x128">
+            <img src="~assets/MAG.png">
+          </figure>
+        </div>
+        <p class="content is-large">
+          <a href="tel:7703782866">
+            (770) 378-2866
+          </a>
+        </p>
+        <p class="content is-large" >
+          <a href="mailto:mike@magcsf.com">
+            sales@magcsf.com
+          </a>
+        </p>
       </div>
     </footer>
   </div>
@@ -71,3 +92,21 @@ export default {
   }
 }
 </script>
+
+<style>
+  .is-horizontal-center {
+    justify-content: center;
+  }
+
+  a {
+    color: #47494e;
+    text-decoration: none;
+  }
+
+  a:hover
+  {
+    color:#7F828B;
+    text-decoration:none;
+    cursor:pointer;
+  }
+</style>
